9087d6b3453b7634575b9f379e70dc8fa0dee16a
KoheiAsami2 <mintomidnight2@gmail.com>
2016-12-11 22:41:36 +0900

    AIクラスの雛形作成

---
+++ Classes/MapObjects/MovePatterns/BattleMob.cpp
@@ -0,0 +1,9 @@
+//
+//  BattleMob.cpp
+//  6chefs2
+//
+//  Created by Kohei Asami on 2016/12/11.
+//
+//
+
+#include "MapObjects/MovePatterns/BattleMob.h"
---
+++ Classes/MapObjects/MovePatterns/BattleMob.h
@@ -0,0 +1,19 @@
+//
+//  BattleMob.h
+//  6chefs2
+//
+//  Created by Kohei Asami on 2016/12/11.
+//
+//
+
+#ifndef BattleMob_h
+#define BattleMob_h
+
+#include "MapObjects/MovePatterns/MovePattern.h"
+
+class BattleMob : public MovePattern
+{
+    
+};
+
+#endif /* BattleMob_h */
