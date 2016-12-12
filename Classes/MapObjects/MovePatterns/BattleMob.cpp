67c4f3a2ce833fad213756257cbea7e474ff9d3f
KoheiAsami2 <mintomidnight2@gmail.com>
2016-12-12 11:22:50 +0900

    AIのインターフェースだけ記述

--- Classes/MapObjects/MovePatterns/BattleMob.cpp
+++ Classes/MapObjects/MovePatterns/BattleMob.cpp
@@ -7,3 +7,42 @@
 //
 
 #include "MapObjects/MovePatterns/BattleMob.h"
+
+#include "MapObjects/MovePatterns/Chaser.h"
+
+// コンストラクタ
+BattleMob::BattleMob() { FUNCLOG }
+
+// デストラクタ
+BattleMob::~BattleMob()
+{
+    FUNCLOG
+    
+    CC_SAFE_RELEASE_NULL(_subPattern);
+}
+
+// 初期化
+bool BattleMob::init(Character* character)
+{
+    if (!MovePattern::init(character)) return false;
+    
+    return true;
+}
+
+
+#pragma mark -
+#pragma mark Interface
+void BattleMob::start()
+{
+    MovePattern::start();
+}
+
+void BattleMob::pause()
+{
+    MovePattern::pause();
+}
+
+void BattleMob::resume()
+{
+    MovePattern::resume();
+}
--- Classes/MapObjects/MovePatterns/BattleMob.h
+++ Classes/MapObjects/MovePatterns/BattleMob.h
@@ -11,9 +11,34 @@
 
 #include "MapObjects/MovePatterns/MovePattern.h"
 
+class Chaser;
+
 class BattleMob : public MovePattern
 {
+// クラスメソッド
+public:
+    CREATE_FUNC_WITH_PARAM(BattleMob, Character*)
     
+// インスタンス変数
+private:
+    Chaser* _subPattern { nullptr };
+    deque<Direction> _path {};
+    
+// インスタンスメソッド
+private:
+    BattleMob();
+    ~BattleMob();
+    virtual bool init(Character* character) override;
+    void move();
+    
+// インターフェース
+public:
+    virtual void start() override;
+    virtual void pause() override;
+    virtual void resume() override;
+    virtual void onPartyMoved() override;
+    virtual bool canGoToNextMap() const override;
+    virtual float calcSummonDelay() const override;
 };
 
 #endif /* BattleMob_h */
