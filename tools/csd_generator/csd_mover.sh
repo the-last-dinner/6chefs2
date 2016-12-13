03f4489276a655df69fde088d93eaedb8067cb1f
inosy22 <inos.sj@gmail.com>
2016-12-14 01:52:49 +0900

    csd_moverを改良

--- tools/csd_generator/csd_mover.sh
+++ tools/csd_generator/csd_mover.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-mv *.csd *.udf *.plist *.png ../../cocosstudio/character/
+if [ ! -e ./targets ]; then
+    echo 'Error: Directory is incorrect.'
+    exit 0
+fi
+mv -i ./targets/* ../../cocosstudio/character
