8687b2ce17ea7a2990822d8d0931865711b0cf87
inosy22 <inos.sj@gmail.com>
2017-02-13 00:35:59 +0900

    オープニング動画とBGMの設定ファイル準備

--- Classes/Models/ConfigData/MasterConfigData.h
+++ Classes/Models/ConfigData/MasterConfigData.h
@@ -28,6 +28,8 @@ public:
     static const char* TWO_ICON;
     static const char* FRIENDSHIP;
     static const char* SPECIAL_ROOM;
+    static const char* OPENING_VIDEO_FILE;
+    static const char* OPENING_BGM_FILE;
     
     // instance valiables
 private:
